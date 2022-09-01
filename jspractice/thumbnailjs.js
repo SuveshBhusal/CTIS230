ddde8467fbed4145d1f9a5682e616fd26a2fd3ff
SuveshBhusal <suvsal8@gmail.com>
2022-09-01 14:41:14 -0400

    gif code

--- jspractice/thumbnailjs.js
+++ jspractice/thumbnailjs.js
@@ -12,15 +12,32 @@ document.addEventListener("keyup", hideBigImage);
 // document.addEventListener("keyup", makeBigImage);
 
 function makeBigImage() {
-  // first be sure that the largeview element has no leftover HTML
-  mybigimage.innerHTML = "";
-  // add an img element as a child of this largeview element
-  bigimage = document.createElement("img");
-  bigimage.src = "2_of_clubs.png";
-  mybigimage.appendChild(bigimage);
-  mybigimage.classList.remove("dontshow");
+    // first be sure that the largeview element has no leftover HTML
+    mybigimage.innerHTML = "";
+    // add an img element as a child of this largeview element
+    bigimage = document.createElement("img");
+    bigimage.src = "2_of_clubs.png";
+    mybigimage.appendChild(bigimage);
+    mybigimage.classList.remove("dontshow");
 }
 
 function hideBigImage() {
-  mybigimage.classList.add("dontshow");
+    mybigimage.classList.add("dontshow");
+}
+
+
+staticimage = document.getElementById("staticview");
+gifimage = document.getElementById("gifview");
+
+staticimage.addEventListener("mouseover", showGIF);
+gifimage.addEventListener("mouseout", hideGif);
+
+function showGIF() {
+    gifview.classList.remove("dontshow");
+    staticview.classList.add("dontshow");
+}
+
+function hideGif() {
+    gifview.classList.add("dontshow");
+    staticview.classList.remove("dontshow");
 }
